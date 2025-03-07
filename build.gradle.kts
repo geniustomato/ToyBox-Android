d35e666ce1cbdcbe9bd28c99d9ce588f7dbc2651
Carlo Trajano <carlo.trajano@gmail.com>
2025-03-07 18:39:38 +0800

    Add Hilt Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.google.dagger.hilt) apply false
 }
